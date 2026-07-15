e09f3111a5206e62e31bffb34e30fd0f36f6330b
Bruno Narciso <61832066+Bruno-21900771@users.noreply.github.com>
2026-07-15 14:06:16 +0000

    Estilização Alunos Curso.html

--- escola_online/urls.py
+++ escola_online/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('cursos/', views.cursos_view, name='cursos_view'),
-    path('', views.cursos_view),  # rota que abre diretamente a página dos cursos
+    path('curso/<int:id>', views.curso_view, name='curso_view'),
+    path('professores/', views.professores_view, name='professores_view'),
+    path('alunos/', views.alunos_view, name='alunos_view'),
+    path('', views.cursos_view),
 ]
--- escola_online/views.py
+++ escola_online/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
-from .models import Curso
+from .models import Curso, Professor, Aluno
 
 
 def cursos_view(request):
     cursos = Curso.objects.select_related('professor').prefetch_related('alunos').all()
     return render(request, 'escola_online/cursos.html', {'cursos': cursos})
+
+def professores_view(request):
+    professores = Professor.objects.prefetch_related('cursos').all()
+    return render(request, 'escola_online/professores.html', {'professores': professores})
+
+def alunos_view(request):
+    alunos = Aluno.objects.prefetch_related('cursos').all()
+    return render(request, 'escola_online/alunos.html', {'alunos': alunos})
+
+def curso_view(request, id):
+    curso = Curso.objects.get(id=id)
+    return render(request, 'escola_online/curso.html', {'curso': curso})
